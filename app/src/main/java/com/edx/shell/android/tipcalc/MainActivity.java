9dfc0c2437b9cef4497116d3a542f437a5b430a4
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-06-02 23:44:32 -0500

    Interfaz.
    Se agregó funcionalidad al botón "Calcular", para que se oculte el teclado automáticamente.

--- app/src/main/java/com/edx/shell/android/tipcalc/MainActivity.java
+++ app/src/main/java/com/edx/shell/android/tipcalc/MainActivity.java
@@ -1,18 +1,29 @@
 package com.edx.shell.android.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Components
+    private Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+        setonClickListeners();
     }
 
     @Override
@@ -26,13 +37,53 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.act_about:
                 about();
+                break;
+            default:
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void initializeComponents() {
+        btnCalculate = (Button) findViewById(R.id.btn_calculate);
+    }
+
+    private void setonClickListeners() {
+        btnCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_calculate:
+                Log.d(getLocalClassName(), "Click en calcular");
+                hideKeyboard();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Función para ocultar el teclado, al dar clic en el botón "Calcular"
+     */
+    private void hideKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            inputMethodManager.hideSoftInputFromInputMethod(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException e) {
+            Log.e(getLocalClassName(), Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * Función para mostrar al usuario información acerca del programador de la aplicación
+     */
     private void about() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(TipCalcApp.ABOUT_URL));
         startActivity(intent);
     }
+
+
 }
