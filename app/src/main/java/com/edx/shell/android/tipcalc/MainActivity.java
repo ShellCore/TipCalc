5308b672365d53d0262d62b140e281e9b8f963ce
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-06-02 23:21:09 -0500

    Interfaz.
    Se agregó menú y opción "Acerca de", el cual dirije al usuario a información acerca del programador.

--- app/src/main/java/com/edx/shell/android/tipcalc/MainActivity.java
+++ app/src/main/java/com/edx/shell/android/tipcalc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.edx.shell.android.tipcalc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.act_about:
+                about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(TipCalcApp.ABOUT_URL));
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/edx/shell/android/tipcalc/TipCalcApp.java
@@ -0,0 +1,10 @@
+package com.edx.shell.android.tipcalc;
+
+import android.app.Application;
+
+/**
+ * Created by Shell on 2016-06-02.
+ */
+public class TipCalcApp extends Application {
+    public static final String ABOUT_URL = "http://shellcore.github.io/Perfil-personal";
+}
