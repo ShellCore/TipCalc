9d7467ff2ce2454568ccc3ce80c78dffb5974a34
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-07 15:36:03 -0500

    Interfaz
    Se creó vista de detalle de petición.

---
+++ app/src/main/java/com/edx/shell/android/tipcalc/activities/TipDetailActivity.java
@@ -0,0 +1,15 @@
+package com.edx.shell.android.tipcalc.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edx.shell.android.tipcalc.R;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
