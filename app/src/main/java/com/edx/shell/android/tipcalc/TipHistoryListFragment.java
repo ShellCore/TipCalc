1781cff6a1da5e25f727c996c57940c313af61b9
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-07 09:25:28 -0500

    Interfaz
    Se creó el fragmento que contendrá la lista de peticiones de propina.

---
+++ app/src/main/java/com/edx/shell/android/tipcalc/TipHistoryListFragment.java
@@ -0,0 +1,25 @@
+package com.edx.shell.android.tipcalc;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TipHistoryListFragment extends Fragment {
+
+
+    public TipHistoryListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tip_history_list, container, false);
+    }
+
+}
