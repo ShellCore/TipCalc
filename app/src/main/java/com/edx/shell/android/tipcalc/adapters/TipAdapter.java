be472ffc700cfea06e63667e768eb64068dc0467
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-07 10:50:16 -0500

    Lógico
    Se agregaron funciones de la lógica del adaptador.

---
+++ app/src/main/java/com/edx/shell/android/tipcalc/adapters/TipAdapter.java
@@ -0,0 +1,63 @@
+package com.edx.shell.android.tipcalc.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.edx.shell.android.tipcalc.R;
+import com.edx.shell.android.tipcalc.models.TipRecord;
+
+import java.util.List;
+
+public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder> {
+
+    private List<TipRecord> recordList;
+    private Context context;
+
+    public TipAdapter(Context context, List<TipRecord> recordList) {
+        this.context = context;
+        this.recordList = recordList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_row, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TipRecord element = recordList.get(position);
+        String strTip = String.format(context.getString(R.string.propina), element.getBill());
+        holder.txtContent.setText(strTip);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recordList.size();
+    }
+
+    public void add(TipRecord record) {
+        recordList.add(record);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        recordList.clear();
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtContent;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txtContent = (TextView) itemView.findViewById(R.id.txt_content);
+        }
+    }
+}
